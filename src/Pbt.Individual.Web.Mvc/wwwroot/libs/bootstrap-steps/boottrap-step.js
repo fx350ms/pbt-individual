3f331fabae7fafc116c0765c64c29d792e0eeab9
NGUYEN THANH TU <thanhtuk9acntt@hotmail.com>
2026-04-20 10:56:15 +0700

    add css

---
+++ src/Pbt.Individual.Web.Mvc/wwwroot/libs/bootstrap-steps/boottrap-step.js
@@ -0,0 +1,16 @@
+﻿const $stepButtons = $('.step-button');
+const $progress = $('#progress');
+
+function setStep(step) {
+    // Cập nhật giá trị của thanh tiến độ
+    $progress.attr('value', step * 100 / ($stepButtons.length - 1));
+
+    // Cập nhật trạng thái của các nút
+    $stepButtons.each(function (index) {
+        if (step >= index) {
+            $(this).addClass('done');
+        } else {
+            $(this).removeClass('done');
+        }
+    });
+}
